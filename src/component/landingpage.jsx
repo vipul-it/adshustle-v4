f3639e4431cdbfca82fb35f7d15e80020f6469ae
vipul <vipulmauryaedu@gmail.com>
2023-06-12 14:05:58 +0530

    7th page fixed

--- src/component/landingpage.jsx
+++ src/component/landingpage.jsx
@@ -3,16 +3,16 @@ import "./landingpage.css";
 import Homepage from "./Homepage";
 
 import Fifthpage from "./fifthpage";
-import Seventhpage from "./seventhpage";
+
 import Team from "./team";
 import Event from "./event";
-import Brands from "./brands";
 
 import Footer from "./footer";
 import Navbar from "./navbar";
 import AboutUs from "./AboutUs";
 import SecondP from "./SecondP";
 import FourthP from "./FourthP";
+import SeventhP from "./SeventhP";
 
 const landingpage = () => {
   return (
@@ -28,11 +28,14 @@ const landingpage = () => {
       <div className="child-section-main">
         <Fifthpage />
       </div>
-      <div className="child-section-main"><FourthP /></div>
-      <div className="child-section-main">{/* <AboutUs /> */}</div>
       <div className="child-section-main">
-        {/* <Seventhpage /> */}
-        {/* <Brands /> */}
+        <FourthP />
+      </div>
+      <div className="child-section-main">
+        <AboutUs />
+      </div>
+      <div className="child-section-main">
+        <SeventhP />
       </div>
       <div className="child-section-main">
         {/* <Team /> */}
